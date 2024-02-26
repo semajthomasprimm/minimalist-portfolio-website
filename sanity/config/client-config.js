02e22bbd9eefb1e23f712d97f289d739c4a3380d
Semaj Primm <semajthomasprimm@users.noreply.github.com>
2024-02-26 00:58:24 -0500

    updated API version

--- sanity/config/client-config.js
+++ sanity/config/client-config.js
@@ -1,7 +1,7 @@
 const config = {
     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
     dataset: "production",
-    apiVersion: "2024-02-25",
+    apiVersion: "2024-02-27",
     useCdn: false
 };
 
